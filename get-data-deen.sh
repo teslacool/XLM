c2a73f47dcd4ff507f4b471289863c729b14cf19
Tesla <1462540095@qq.com>
2019-07-19 05:26:55 +0000

    get-data-deen

---
+++ get-data-deen.sh
@@ -0,0 +1,163 @@
+#!/usr/bin/env bash
+
+set -x
+set -e
+
+N_MONO=5000000  # number of monolingual sentences for each language
+CODES=60000     # number of BPE codes
+N_THREADS=16    # number of threads in data preprocessing
+
+
+
+SRC=de
+TGT=en
+RELOAD_CODES=codes_ende
+RELOAD_VOCAB=vocab_ende
+
+#
+# Check parameters
+#
+if [ "$SRC" == "" ]; then echo "--src not provided"; exit; fi
+if [ "$TGT" == "" ]; then echo "--tgt not provided"; exit; fi
+if [ "$SRC" != "de" -a "$SRC" != "en" -a "$SRC" != "fr" -a "$SRC" != "ro" ]; then echo "unknown source language"; exit; fi
+if [ "$TGT" != "de" -a "$TGT" != "en" -a "$TGT" != "fr" -a "$TGT" != "ro" ]; then echo "unknown target language"; exit; fi
+if [ "$SRC" == "$TGT" ]; then echo "source and target cannot be identical"; exit; fi
+if [ "$SRC" \> "$TGT" ]; then echo "please ensure SRC < TGT"; exit; fi
+if [ "$RELOAD_CODES" != "" ] && [ ! -f "$RELOAD_CODES" ]; then echo "cannot locate BPE codes"; exit; fi
+if [ "$RELOAD_VOCAB" != "" ] && [ ! -f "$RELOAD_VOCAB" ]; then echo "cannot locate vocabulary"; exit; fi
+if [ "$RELOAD_CODES" == "" -a "$RELOAD_VOCAB" != "" -o "$RELOAD_CODES" != "" -a "$RELOAD_VOCAB" == "" ]; then echo "BPE codes should be provided if and only if vocabulary is also provided"; exit; fi
+
+
+#
+# Initialize tools and data paths
+#
+
+# main paths
+MAIN_PATH=$PWD
+TOOLS_PATH=$PWD/tools
+DATA_PATH=$PWD/data
+PARA_PATH=$DATA_PATH/para
+PROC_PATH=$DATA_PATH/processed/$SRC-$TGT
+
+# create paths
+mkdir -p $TOOLS_PATH
+mkdir -p $DATA_PATH
+mkdir -p $PARA_PATH
+mkdir -p $PROC_PATH
+
+# moses
+MOSES=$TOOLS_PATH/mosesdecoder
+REPLACE_UNICODE_PUNCT=$MOSES/scripts/tokenizer/replace-unicode-punctuation.perl
+NORM_PUNC=$MOSES/scripts/tokenizer/normalize-punctuation.perl
+REM_NON_PRINT_CHAR=$MOSES/scripts/tokenizer/remove-non-printing-char.perl
+TOKENIZER=$MOSES/scripts/tokenizer/tokenizer.perl
+INPUT_FROM_SGM=$MOSES/scripts/ems/support/input-from-sgm.perl
+
+# fastBPE
+FASTBPE_DIR=$TOOLS_PATH/fastBPE
+FASTBPE=$TOOLS_PATH/fastBPE/fast
+
+# Sennrich's WMT16 scripts for Romanian preprocessing
+WMT16_SCRIPTS=$TOOLS_PATH/wmt16-scripts
+NORMALIZE_ROMANIAN=$WMT16_SCRIPTS/preprocess/normalise-romanian.py
+REMOVE_DIACRITICS=$WMT16_SCRIPTS/preprocess/remove-diacritics.py
+
+# raw and tokenized files
+SRC_TRAIN=$PARA_PATH/train.$SRC
+TGT_TRAIN=$PARA_PATH/train.$TGT
+SRC_VALID=$PARA_PATH/valid.$SRC
+TGT_VALID=$PARA_PATH/valid.$TGT
+SRC_TEST=$PARA_PATH/test.$SRC
+TGT_TEST=$PARA_PATH/test.$TGT
+
+# BPE / vocab files
+BPE_CODES=$PROC_PATH/codes
+SRC_VOCAB=$PROC_PATH/vocab.$SRC
+TGT_VOCAB=$PROC_PATH/vocab.$TGT
+FULL_VOCAB=$PROC_PATH/vocab.$SRC-$TGT
+
+# train / valid / test monolingual BPE data
+SRC_TRAIN_BPE=$PROC_PATH/train.$SRC-$TGT.$SRC
+TGT_TRAIN_BPE=$PROC_PATH/train.$SRC-$TGT.$TGT
+SRC_VALID_BPE=$PROC_PATH/valid.$SRC-$TGT.$SRC
+TGT_VALID_BPE=$PROC_PATH/valid.$SRC-$TGT.$TGT
+SRC_TEST_BPE=$PROC_PATH/test.$SRC-$TGT.$SRC
+TGT_TEST_BPE=$PROC_PATH/test.$SRC-$TGT.$TGT
+
+
+
+# install tools
+./install-tools.sh
+
+
+
+# preprocessing commands - special case for Romanian
+if [ "$SRC" == "ro" ]; then
+  SRC_PREPROCESSING="$REPLACE_UNICODE_PUNCT | $NORM_PUNC -l $SRC | $REM_NON_PRINT_CHAR | $NORMALIZE_ROMANIAN | $REMOVE_DIACRITICS | $TOKENIZER -l $SRC -no-escape -threads $N_THREADS"
+else
+  SRC_PREPROCESSING="$REPLACE_UNICODE_PUNCT | $NORM_PUNC -l $SRC | $REM_NON_PRINT_CHAR |                                            $TOKENIZER -l $SRC -no-escape -threads $N_THREADS"
+fi
+if [ "$TGT" == "ro" ]; then
+  TGT_PREPROCESSING="$REPLACE_UNICODE_PUNCT | $NORM_PUNC -l $TGT | $REM_NON_PRINT_CHAR | $NORMALIZE_ROMANIAN | $REMOVE_DIACRITICS | $TOKENIZER -l $TGT -no-escape -threads $N_THREADS"
+else
+  TGT_PREPROCESSING="$REPLACE_UNICODE_PUNCT | $NORM_PUNC -l $TGT | $REM_NON_PRINT_CHAR |                                            $TOKENIZER -l $TGT -no-escape -threads $N_THREADS"
+fi
+
+
+# reload BPE codes
+cd $MAIN_PATH
+if [ ! -f "$BPE_CODES" ] && [ -f "$RELOAD_CODES" ]; then
+  echo "Reloading BPE codes from $RELOAD_CODES ..."
+  cp $RELOAD_CODES $BPE_CODES
+fi
+
+# learn BPE codes
+if [ ! -f "$BPE_CODES" ]; then
+  echo "Learning BPE codes..."
+  $FASTBPE learnbpe $CODES $SRC_TRAIN $TGT_TRAIN > $BPE_CODES
+fi
+echo "BPE learned in $BPE_CODES"
+
+# apply BPE codes
+if ! [[ -f "$SRC_TRAIN_BPE" ]]; then
+  echo "Applying $SRC BPE codes..."
+  $FASTBPE applybpe $SRC_TRAIN_BPE $SRC_TRAIN $BPE_CODES
+fi
+if ! [[ -f "$TGT_TRAIN_BPE" ]]; then
+  echo "Applying $TGT BPE codes..."
+  $FASTBPE applybpe $TGT_TRAIN_BPE $TGT_TRAIN $BPE_CODES
+fi
+
+
+# reload full vocabulary
+cd $MAIN_PATH
+if [ ! -f "$FULL_VOCAB" ] && [ -f "$RELOAD_VOCAB" ]; then
+  echo "Reloading vocabulary from $RELOAD_VOCAB ..."
+  cp $RELOAD_VOCAB $FULL_VOCAB
+fi
+
+# extract full vocabulary
+if ! [[ -f "$FULL_VOCAB" ]]; then
+  echo "Extracting vocabulary..."
+  $FASTBPE getvocab $SRC_TRAIN_BPE $TGT_TRAIN_BPE > $FULL_VOCAB
+fi
+echo "Full vocab in: $FULL_VOCAB"
+
+
+
+echo "Applying BPE to valid and test files..."
+$FASTBPE applybpe $SRC_VALID_BPE $SRC_VALID $BPE_CODES
+$FASTBPE applybpe $TGT_VALID_BPE $TGT_VALID $BPE_CODES
+$FASTBPE applybpe $SRC_TEST_BPE  $SRC_TEST  $BPE_CODES
+$FASTBPE applybpe $TGT_TEST_BPE  $TGT_TEST  $BPE_CODES
+
+echo "Binarizing data..."
+$MAIN_PATH/preprocess.py $FULL_VOCAB $SRC_TRAIN_BPE
+$MAIN_PATH/preprocess.py $FULL_VOCAB $TGT_TRAIN_BPE
+$MAIN_PATH/preprocess.py $FULL_VOCAB $SRC_VALID_BPE
+$MAIN_PATH/preprocess.py $FULL_VOCAB $TGT_VALID_BPE
+$MAIN_PATH/preprocess.py $FULL_VOCAB $SRC_TEST_BPE
+$MAIN_PATH/preprocess.py $FULL_VOCAB $TGT_TEST_BPE
+
+
+
